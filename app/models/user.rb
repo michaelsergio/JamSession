ef1d936ac75df8e704ae4493197e7b4599b14b02
Michael Sergio <mikeserg@gmail.com>
2012-05-17 09:45:17 -0400

    now have the ability to add skills, but not update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,18 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :name, :location, :hide_location_from_search, :about
 
+  has_attached_file :profile_picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  attr_accessible  :profile_picture
+
   validates_presence_of :name
   
   has_many :personal_skills
   has_many :personal_styles
-  has_many :skills, through: :personal_skills
-  has_many :styles, through: :personal_styles
-
-  attr_accessible :skills, :styles
-
-  has_attached_file :profile_picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  attr_accessible  :profile_picture
+  has_many :skills, through: :personal_skills, dependent: :destroy
+  has_many :styles, through: :personal_styles, dependent: :destroy
+  
+  accepts_nested_attributes_for :personal_skills
+  accepts_nested_attributes_for :personal_styles
 
   acts_as_messageable
 
