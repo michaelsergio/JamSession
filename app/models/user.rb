1f51c87834e631e115bf9c66d518e7de08a1ca68
Michael Sergio <mikeserg@gmail.com>
2012-05-16 23:38:21 -0400

    added about to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :name, :location, :hide_location_from_search
+  attr_accessible :name, :location, :hide_location_from_search, :about
 
   validates_presence_of :name
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120514233100) do
+ActiveRecord::Schema.define(:version => 20120517033013) do
 
   create_table "messages", :force => true do |t|
     t.string   "topic"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(:version => 20120514233100) do
     t.datetime "updated_at", :null => false
   end
 
+  add_index "styles", ["name"], :name => "index_styles_on_name"
+
   create_table "users", :force => true do |t|
     t.string   "email",                        :default => "", :null => false
     t.string   "encrypted_password",           :default => "", :null => false
@@ -113,9 +115,11 @@ ActiveRecord::Schema.define(:version => 20120514233100) do
     t.float    "latitude"
     t.float    "longitude"
     t.boolean  "hide_location_from_search"
+    t.text     "about"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "welcomes", :force => true do |t|
